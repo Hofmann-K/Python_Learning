9b88f5c8c7c5328ea8fd3e7cf2ff2d36f9f08dfe
Hofmann-K <kevin@planethofmann.com>
2025-10-27 20:09:33 -0400

    Inital commit - setup first python files

---
+++ milestone_1/lesson_1/hello_world.py
@@ -0,0 +1,5 @@
+'''
+Milestone 1, Lesson 1: First python file
+'''
+
+print("Hello World!")
---
+++ milestone_1/lesson_1/introduce_yourself.py
@@ -0,0 +1,7 @@
+'''
+Milestone 1, Lesson 1: Introduction
+'''
+
+print("Hello, my name is Kevin!")
+print("I am learning python to improve my programming ability")
+print("I would like to create any projects involvign what I find interesting")
